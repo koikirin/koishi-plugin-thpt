7fcc8216c4143e94151704c016b22cd2dd9e160d
Hieuzest <girkirin@hotmail.com>
2024-02-14 21:02:05 +0800

    feat: support i18n

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Session } from 'koishi'
 
 declare module 'koishi' {
   interface User {
@@ -22,43 +22,45 @@ export const Config: Schema<Config> = Schema.object({
 
 const TREND_VALID_TIME = 60 * 60 * 24 * 30
 
-function generateReply(ranks: any) {
+function generateReply(session: Session, ranks: any) {
   let msg = ranks.description
   const curtime = Date.now() / 1000
-  if (ranks.h4.level >= 16 || ranks.h3.level >= 16) msg += '\n最高段位 ' + ranks.hdescription
+  if (ranks.h4.level >= 16 || ranks.h3.level >= 16) msg += `\n${session.text('.highest-level')} ` + ranks.hdescription
 
   let trend4 = '', trend3 = ''
   if (ranks['4'].time_last > curtime - TREND_VALID_TIME && ranks['4'].trend?.length > 10) trend4 = ranks['4'].trend.slice(-10)
   if (ranks['3'].time_last > curtime - TREND_VALID_TIME && ranks['3'].trend?.length > 10) trend3 = ranks['3'].trend.slice(-10)
-  if (trend4 || trend3) msg += `\n最近战绩 [${trend4}][${trend3}]`
+  if (trend4 || trend3) msg += `\n${session.text('.recent-results')} [${trend4}][${trend3}]`
 
-  if (ranks.h4.level >= 16) msg += '\n四麻首次升凤时间: ' + new Date(1000 * ranks['4'].time_phoenix).toLocaleDateString()
-  if (ranks.h4.level >= 17) msg += '\n最高段位达成时间: ' + new Date(1000 * ranks['h4'].time).toLocaleDateString()
-  if (ranks.h3.level >= 16) msg += '\n三麻首次升凤时间: ' + new Date(1000 * ranks['3'].time_phoenix).toLocaleDateString()
-  if (ranks.h3.level >= 17) msg += '\n最高段位达成时间: ' + new Date(1000 * ranks['h3'].time).toLocaleDateString()
+  if (ranks.h4.level >= 16) msg += `\n${session.text('.phoenix-time-4')}: ` + new Date(1000 * ranks['4'].time_phoenix).toLocaleDateString()
+  if (ranks.h4.level >= 17) msg += `\n${session.text('.highest-time-4')}: ` + new Date(1000 * ranks['h4'].time).toLocaleDateString()
+  if (ranks.h3.level >= 16) msg += `\n${session.text('.phoenix-time-3')}: ` + new Date(1000 * ranks['3'].time_phoenix).toLocaleDateString()
+  if (ranks.h3.level >= 17) msg += `\n${session.text('.highest-time-3')}: ` + new Date(1000 * ranks['h3'].time).toLocaleDateString()
   return msg
 }
 
 export function apply(ctx: Context, config: Config) {
+  ctx.i18n.define('zh-CN', require('./locales/zh-CN'))
+
   ctx.model.extend('user', {
     'thpt/bind': 'string',
   })
 
-  ctx.command('thpt [username:rawtext]', '查询天凤PT')
+  ctx.command('thpt [username:rawtext]')
     .option('source', '-s <source>', { fallback: config.defaultSource })
-    .option('source', '-n', { value: 'nodocchi', descPath: '数据有误时使用此选项同步' })
-    .option('bind', '-b', { descPath: '绑定至当前用户' })
+    .option('source', '-n', { value: 'nodocchi', descPath: 'commands.thpt.options.source-nodocchi' })
+    .option('bind', '-b')
     .userFields(['thpt/bind'])
     .action(async ({ session, options }, username) => {
       if (options.bind) session.user['thpt/bind'] = username ?? ''
       username ||= session.user['thpt/bind']
-      if (!username) return session.execute('thpt -h')
+      if (!username) return options.bind ? '' : session.execute('thpt -h')
       const res = await ctx.http.get(`${config.server}/rank`, {
         params: {
           username,
           source: options.source,
         },
       }).catch(_ => null)
-      return res ? generateReply(res) : '查询失败'
+      return res ? generateReply(session, res) : session.text('.failed')
     })
 }
